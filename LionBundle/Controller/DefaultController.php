7b5ccd5e7ff8377d1d98224a9243cdd35cd94db0
lion115 <56legona@gmail.com>
2016-11-16 18:39:13 +0200

    test bundle

---
+++ LionBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace LionBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('LionBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
---
+++ LionBundle/LionBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace LionBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class LionBundle extends Bundle
+{
+}
